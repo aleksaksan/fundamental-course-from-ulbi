9a9ce7704d2f2984208fd42f62cc7fb1dbd7cc3a
Aleksander <sanyok.10rus@gmail.com>
2022-06-24 07:53:43 +0300

    added fetch request from axios lib and added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList/PostList';
 import PostForm from './components/PostForm/PostForm';
@@ -6,20 +6,29 @@ import PostFilter from './components/PostFilter/PostFilter';
 import { Button } from './components/UI/Button/Button';
 import { ModalWindow } from './components/ModalWindow/ModalWindow';
 import { usePosts } from './hooks/usePosts';
+import PostService from './API/PostService/PostService';
+import Loader from './components/Loader/Loader';
 
 export const App = () => {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: '3 JS - is a programming language!'},
-    {id: 2, title: 'Python', body: '2 Python - is a programming language!'},
-    {id: 3, title: 'C#', body: '1 C# - is a programming language!'}
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [isModalActive, setIsModalActive] = useState(false);
   //  выбранный алгоритм сортировки и поисковая строка
   const [filter, setFilter] = useState({sort: '', query: ''});
+  const [isLoading, setIsLoading] = useState(false);
   //используем свой хук
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+  const fetchPosts = async () => {
+    setIsLoading(true);
+    const response = await PostService.getAll();
+    setPosts(response);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  
   //  callback, для получения нового поста в  массиве
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -44,7 +53,10 @@ export const App = () => {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts List:" />
+      {isLoading ? 
+        <Loader /> :
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts List:" />
+      }
     </div>
   );
 }
